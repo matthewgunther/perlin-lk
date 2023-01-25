9c5d5e7700223e539a714d77b0caa6c1c02c5d7d
Matthew Gunther <mgunther26@gmail.com>
2023-01-25 10:04:20 -0500

    optical flow working

--- src/lk.cpp
+++ src/lk.cpp
@@ -2,12 +2,20 @@
 #include <opencv4/opencv2/opencv.hpp>
 #include "../include/particle.h"
 
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+// #include <cstdlib>
+
+
 using namespace std;
 using namespace cv;
 
 
-float scale { 4 };
-int window_dim { 4 };
+float scale { 10 };
+float flow_threshold { 5 };
+int window_dim { 10 };
+
 
 
 Mat resize_image (Mat input_image, float scale) {
@@ -36,9 +44,61 @@ void initialize_windows(string (&windows)[N]) {
 }
 
 
-int main () {
+float map_atan_to_360_deg(float x, float y, float angle) {
+    if (x > 0) {
+        if (angle < 0) {
+            angle += 360.0f;
+        }
+    } else {
+        angle += 180.0f;
+    }
+    return angle;
+}
 
-    // Mat fr = Mat::zeros(100, 200, CV_8UC3);
+Vec3b get_rgb_from_hsv(float angle) {
+
+    Vec3b color;
+    
+    float H = angle / 60.0f;
+    float V = 1.0f;
+    float S = 1.0f;
+
+    float C = V * S;
+    int X_rgb = (int)(C * (1 - fabs(fmod(H, 2.0f) - 1)) * 255.0f);
+    int C_rgb = (int)(C * 255.0f);
+
+    if (H >= 0 & H < 1) {
+        color[0] = C_rgb;
+        color[1] = X_rgb;
+        color[2] = 0;
+    } else if (H >= 1 & H < 2) {
+        color[0] = X_rgb;
+        color[1] = C_rgb;
+        color[2] = 0;
+    } else if (H >= 2 & H < 3) {
+        color[0] = 0;
+        color[1] = C_rgb;
+        color[2] = X_rgb;
+    } else if (H >= 3 & H < 4) {
+        color[0] = 0;
+        color[1] = X_rgb;
+        color[2] = C_rgb;
+    } else if (H >= 4 & H < 5) {
+        color[0] = X_rgb;
+        color[1] = 0;
+        color[2] = C_rgb;
+    } else if (H >= 5 & H < 6) {
+        color[0] = C_rgb;
+        color[1] = 0;
+        color[2] = X_rgb;
+    }
+    
+
+    return color;
+
+}
+
+int main () {
 
     Particle bub = Particle(
         5, 10,
@@ -74,7 +134,7 @@ int main () {
         "Frame",
         "LK X",
         "LK Y",
-        "bub"
+        "flow"
     };
 
     initialize_windows(windows);
@@ -106,17 +166,22 @@ int main () {
         if (previous_frame.empty()) {
             previous_frame = current_frame.clone();
             previous_frame_scaled = resize_image(previous_frame, scale);
+            previous_frame_scaled.convertTo(previous_frame_scaled, CV_32FC1);
         }
 
         Mat current_frame_scaled = resize_image(current_frame, scale);
+        current_frame_scaled.convertTo(current_frame_scaled, CV_32FC1);
+
+        Mat t_gradient;
+        subtract(current_frame_scaled, previous_frame_scaled, t_gradient);
+
+
 
         Mat x_gradient;
         Mat y_gradient;
         filter2D(current_frame_scaled, x_gradient, -1 , x_kernel, Point( -1, -1 ), 0, BORDER_DEFAULT);
         filter2D(current_frame_scaled, y_gradient, -1 , y_kernel, Point( -1, -1 ), 0, BORDER_DEFAULT);
 
-        Mat t_gradient;
-        subtract(current_frame_scaled, previous_frame_scaled, t_gradient);
 
 
         
@@ -154,6 +219,12 @@ int main () {
 
                 u.at<float>(r, c) = nu.at<float>(0, 0);
                 v.at<float>(r, c) = nu.at<float>(1, 0);
+
+                // cout << "Ax \n" << Ax << endl;
+                // cout << "Ay \n" << Ay << endl;
+                // cout << "b \n" << b << endl;
+
+
             }
         }
 
@@ -171,25 +242,103 @@ int main () {
         // int window_height { 500 };
         // Mat frame_bub = Mat::zeros(window_height, window_width, CV_8UC3);
 
-        int window_width = current_frame.cols;
-        int window_height = current_frame.rows;
-        Mat frame_bub = Mat::zeros(window_height, window_width, CV_8UC3);
-        circle(frame_bub, Point(int(bub.pos_x), int(bub.pos_y)), 20, Scalar(0, 255, 0), -1);
-        imshow("bub", frame_bub);
+        // int window_width = current_frame.cols;
+        // int window_height = current_frame.rows;
+        // Mat frame_bub = Mat::zeros(window_height, window_width, CV_8UC3);
+        // circle(frame_bub, Point(int(bub.pos_x), int(bub.pos_y)), 20, Scalar(0, 255, 0), -1);
+        // imshow("bub", frame_bub);
 
-        bub.vel_x = bub.vel_x + u.at<float>(int(bub.pos_x / scale), int(bub.pos_y / scale)) * 10;
-        // bub.vel_y = u.at<float>(bub.pos_x, bub.pos_y) * 5;
-        bub.update(0.2, window_width, window_height);
-        cout << bub.vel_x  << " - " << bub.vel_y <<  endl;
-
-
+        // bub.vel_x = bub.vel_x + u.at<float>(int(bub.pos_x / scale), int(bub.pos_y / scale)) * 10;
+        // // bub.vel_y = u.at<float>(bub.pos_x, bub.pos_y) * 5;
+        // bub.update(0.2, window_width, window_height);
+        // cout << bub.vel_x  << " - " << bub.vel_y <<  endl;
 
 
-        u.convertTo(u, CV_8UC1);
-        v.convertTo(v, CV_8UC1);
-        show_image("LK X", u);
-        show_image("LK Y", v);
 
+
+        // u.convertTo(u, CV_8UC1);
+        // v.convertTo(v, CV_8UC1);
+
+        Mat flow = Mat::zeros(current_frame_scaled.rows, current_frame_scaled.cols, CV_8UC3);
+        for (int r=0; r < current_frame_scaled.rows; r++) {
+            for (int c=0; c < current_frame_scaled.cols; c++) {
+                float x = u.at<float>(r, c);
+                float y = v.at<float>(r, c);
+
+
+                if (sqrtf32(pow(x, 2) + pow(y, 2)) > flow_threshold) {
+
+                
+
+                    float angle = atanf32(y / x) * 180 / M_PI;
+                    angle = map_atan_to_360_deg(x, y, angle); // maps arctan output to 360 degrees
+                    Vec3b color = get_rgb_from_hsv(angle);
+
+                    // cout << x << " / " << y << " / " << angle << " / " << color << endl;
+
+                    flow.at<Vec3b>(r, c) = color;
+                }
+            }
+        }
+
+        // cout << "0" << " - " << get_rgb_from_hsv(0) << endl;
+        // cout << "90" << " - " << get_rgb_from_hsv(90) << endl;
+        // cout << "180" << " - " << get_rgb_from_hsv(180) << endl;
+        // cout << "360" << " - " << get_rgb_from_hsv(270) << endl;
+        // cout << v.rows << " - " << v.cols << endl;
+
+        for (int r=0; r<3; r++) {
+            for (int c=0; c<36; c++) {
+                flow.at<Vec3b>(r, c) = get_rgb_from_hsv((float)(c * 10));
+            }
+        }
+
+
+        flow = resize_image(flow, 1/scale);
+        flow = resize_image(flow, 1.2);
+        current_frame = resize_image(current_frame, 1.2);
+        imshow("flow", flow);
+        imshow("Frame", current_frame);
+
+
+
+        // // flow = resize_image(flow, 1 / scale);
+        // // current_frame.convertTo(current_frame, CV_8UC3);
+
+        // // cout << flow.rows  << " - " << flow.cols <<  endl;
+        // // cout << current_frame.rows  << " - " << current_frame.cols <<  endl;
+
+        // // Mat sub;
+        // // subtract(current_frame, flow, sub);
+
+        // Mat foreground = flow;
+        // Mat background = current_frame_scaled;
+        // Mat alpha = flow;
+
+        // // Convert Mat to float data type
+        // foreground.convertTo(foreground, CV_32FC3);
+        // background.convertTo(background, CV_32FC3);
+
+        // // Normalize the alpha mask to keep intensity between 0 and 1
+        // alpha.convertTo(alpha, CV_32FC3, 1.0/255); // 
+
+        // // Storage for output image
+        // Mat ouImage = Mat::zeros(foreground.size(), foreground.type());
+
+        // // Multiply the foreground with the alpha matte
+        // multiply(alpha, foreground, foreground); 
+        // // Multiply the background with ( 1 - alpha )
+        // multiply(Scalar::all(1.0)-alpha, background, background); 
+
+        // // Add the masked foreground and background.
+        // add(foreground, background, ouImage); 
+
+        // // Display image
+        // imshow("alpha blended image", ouImage/255);
+
+        
+        // // show_image("flow", flow);
+ 
 
 
         char key_press = waitKey(10);
